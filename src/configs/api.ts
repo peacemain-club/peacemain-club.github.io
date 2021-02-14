a57fbac1d6b14930478dc75fdfa5f1081710de7c
edegiil <electropark917@gmail.com>
2021-02-15 08:06:00 +0900

    Update: API 엔드포인트 수정

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,5 +1,4 @@
-// const root = process.env.REACT_APP_API_ROOT_ENDPOINT;
-const root = 'http://localhost:4000';
+const root = process.env.REACT_APP_API_ROOT_ENDPOINT;
 
 export const ENDPOINT_SUBMIT = `${root}/apply`;
 export const ENDPOINT_LOGIN = `${root}/login`;
