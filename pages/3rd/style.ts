14a13dc54d2b536c90dc1d578d7546b5fc208a86
edegil <electropark917@gmail.com>
2022-04-21 23:45:21 +0900

    style: 3rd page

---
+++ pages/3rd/style.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const Page = styled.div`
+  justify-content: center;
+
+  height: 100%;
+  width: 100%;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BannerSection = styled.section``;
+
+export const MainSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+
+  width: 900px;
+
+  margin-bottom: 52px;
+
+  ${({theme}) => theme.break_points.TABLET} {
+    width: 480px;
+    padding: 16px;
+  }
+  ${({theme}) => theme.break_points.MOBILE} {
+    width: 100vw;
+    padding: 16px;
+  }
+`;
+
+export const MainSectionTitleWrapper = styled.div`
+  margin-bottom: 16px;
+`;
+
+export const MainSectionTitle = styled.h2`
+  font-size: ${({theme}) => theme.font_size.R300};
+  font-weight: 100;
+
+  ${({theme}) => theme.break_points.MOBILE} {
+    font-size: ${({theme}) => theme.font_size.R200};
+  }
+`;
+
+export const MainSectionSubtitle = styled.h6`
+  font-size: ${({theme}) => theme.font_size.R100};
+`;
+
+export const MainSectionText = styled.p``;
+
+export const MainSectionList = styled.ul`
+  line-height: 20px;
+`;
+
+export const MainSectionListItem = styled.li`
+  list-style-type: disc;
+  list-style-position: inside;
+`;
