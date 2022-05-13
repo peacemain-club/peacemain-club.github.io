d57a2f24cdea3c1d62f8d36512a20dd9b5223579
edegiil <electropark917@gmail.com>
2022-05-13 21:49:25 +0900

    chore: modify page extension and webpack config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,19 @@
 const debug = process.env.NODE_ENV !== 'production';
 
 const nextConfig = {
+  pageExtensions: ['tsx'],
   reactStrictMode: true,
   assetPrefix: !debug ? '/peacemain-club.github.io/' : '',
   compiler: {
     styledComponents: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
 
