28531d906723aafaf938b17881d8a87729e7ac7a
edegiil <electropark917@gmail.com>
2022-05-14 22:09:22 +0900

    Updates

--- _next/static/chunks/pages/index-b4646cfbf6506cb1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5301:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(4186)}])},4186:function(n,u,t){"use strict";t.r(u),t.d(u,{default:function(){return _.default}});var _=t(8707)}},function(n){n.O(0,[241,5,843,707,774,888,179],(function(){return u=5301,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
